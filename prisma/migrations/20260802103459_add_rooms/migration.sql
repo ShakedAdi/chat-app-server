841aec80f53a19677e11c793ccb2b5e27171c3f5
ShakedAdi <shakedadi2512@gmail.com>
2026-08-02 13:43:00 +0300

    feat: add enums MemberRole, RoomType and models Room, RoomMember

---
+++ prisma/migrations/20260802103459_add_rooms/migration.sql
@@ -0,0 +1,43 @@
+-- CreateEnum
+CREATE TYPE "MemberRole" AS ENUM ('ADMIN', 'MEMBER');
+
+-- CreateEnum
+CREATE TYPE "RoomType" AS ENUM ('DIRECT', 'GROUP');
+
+-- CreateTable
+CREATE TABLE "RoomMember" (
+    "id" TEXT NOT NULL,
+    "roomId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "role" "MemberRole" NOT NULL DEFAULT 'MEMBER',
+    "joinedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "RoomMember_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Room" (
+    "id" TEXT NOT NULL,
+    "type" "RoomType" NOT NULL,
+    "dmKey" TEXT,
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "RoomMember_userId_idx" ON "RoomMember"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RoomMember_roomId_userId_key" ON "RoomMember"("roomId", "userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Room_dmKey_key" ON "Room"("dmKey");
+
+-- AddForeignKey
+ALTER TABLE "RoomMember" ADD CONSTRAINT "RoomMember_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RoomMember" ADD CONSTRAINT "RoomMember_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
