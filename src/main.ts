8b1de6f158c97e69d5bf4099c21919b115bcf601
ShakedAdi <shakedadi2512@gmail.com>
2026-08-07 13:00:03 +0300

    refactor: enable cors to allow the origin localhost:5173

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
 import { load } from 'js-yaml';
@@ -8,6 +9,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+
+  const allowedOrigin = config.get<string>(
+    'ALLOWED_ORIGIN',
+    'http://localhost:5173',
+  );
+  app.enableCors({ origin: allowedOrigin });
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
