4907dcde9693de0990f41e9c3e63896f02826488
ShakedAdi <shakedadi2512@gmail.com>
2026-08-07 12:41:30 +0300

    refactor: remove isPrismaError function

--- src/prisma/prisma-errors.ts
+++
@@ -1,7 +0,0 @@
-import { Prisma } from '../generated/prisma/client';
-
-export const isPrismaError = (
-  e: unknown,
-  code: string,
-): e is Prisma.PrismaClientKnownRequestError =>
-  e instanceof Prisma.PrismaClientKnownRequestError && e.code === code;
